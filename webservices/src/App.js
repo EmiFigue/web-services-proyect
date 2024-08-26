21a7344d62b1715e05cceaa1359c77775cbcb091
Emifigueroa <emifigueroarojas@ciencias.unam.mx>
2024-08-26 16:14:52 -0600

    API no funcional, pero estructura correcta

--- webservices/src/App.js
+++ webservices/src/App.js
@@ -1,15 +1,24 @@
 import './assets/App.css';
+import { useState } from "react";
 
-const weather_api = {
+const api = {
   key:'39942a3074e7466c372775ab00ba986d',
-  base:'http://api.openweathermap.org/data/2.5/'
+  base:'https://openweathermap.org/'
 };
 function App() {
+  const [search, setSearch] = useState("");
+  const searchPressed = () => {
+    fetch('${api.base}weather?q=${search}&units=metric&APPID=${api.key}')
+    .then((result) => {console.log(result);});
+  }
   return (
     <div className="App">
       <header className="App-header">
-      <input type='text' placeholder='Introduce ciudad'/>
-      
+      <input type='text' placeholder='Introduce ciudad'
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={searchPressed}>search</button>
+      <p> Mexico City</p>
       </header>
     </div>
   );
