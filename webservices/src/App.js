5b3aa1656c3f5225ee3c74146ce2f6ca3747b23b
Emifigueroa <emifigueroarojas@ciencias.unam.mx>
2024-08-27 12:21:28 -0600

    API funcional y regresa json

--- webservices/src/App.js
+++ webservices/src/App.js
@@ -3,14 +3,15 @@ import { useState } from "react";
 
 const api = {
   key:'39942a3074e7466c372775ab00ba986d',
-  base:'https://openweathermap.org/'
+  base:'http://api.openweathermap.org/data/2.5/'
 };
 function App() {
   const [search, setSearch] = useState("");
   const searchPressed = () => {
-    fetch('${api.base}weather?q=${search}&units=metric&APPID=${api.key}')
+    fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}`)
+    .then((result) => result.json())
     .then((result) => {console.log(result);});
-  }
+  };
   return (
     <div className="App">
       <header className="App-header">
