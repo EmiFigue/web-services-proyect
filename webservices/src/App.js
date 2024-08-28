d4598cf0c25d0a02e1075050ccf0b5558064f6b0
Lara <laronious@ciencias.unam.mx>
2024-08-28 11:21:39 -0600

    Se agregó el manejo de errores al obtener datos de la API OpenWeatherMap. Ahora se muestran íconos correspondientes a la descripción del clima.

--- webservices/src/App.js
+++ webservices/src/App.js
@@ -12,21 +12,30 @@ function App() {
     fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}&lang=es`)
     .then((result) => result.json())
     .then((res) => (setWeather(res)))
+    .catch((error) => console.error('Error al obtener los datos del clima:', error));
+    
   };
   return (
     <div className="App">
       <header className="App-header">
-      <input type='text' placeholder='Introduce ciudad'
-        onChange={(e) => setSearch(e.target.value)}
+        <input 
+          type='text'  
+          placeholder='Introduce ciudad'
+          onChange={(e) => setSearch(e.target.value)}
       />
       <button onClick={searchPressed}>search</button>
       {typeof weather.main == "undefined" ? 
       ("") : 
-      (<div>
+      (
+      <div>
         <p> Ciudad destino {weather.name} </p>
         <p> {weather.weather[0].description} </p>
         <p> Temperatura {weather.main.temp}°C </p>
         <p> Sensacion termica {weather.main.feels_like}°C</p>
+        <img 
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+              alt="Icon" 
+        />
       </div>)
       }
       </header>   
