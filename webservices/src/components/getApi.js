651fbda02a82f0b0a857d8d35f0fd2a601c56fa9
AlejandroHerreraAvalosFC <alejandroahafc@ciencias.unam.mx>
2024-09-05 13:29:02 -0600

    Se modifico el fetch y se agrega test de getApi

--- webservices/src/components/getApi.js
+++ webservices/src/components/getApi.js
@@ -2,7 +2,13 @@ const api = {
     key:'39942a3074e7466c372775ab00ba986d',
     base:'http://api.openweathermap.org/data/2.5/'
   };
+const validar= require ("./validarString");
   export const fetchWeather = (search) => {
-    return fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}&lang=es`)
+    let controller= new AbortController();
+    let signal = controller.signal;
+    if(validar.validarString(search)){
+      return fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}&lang=es`, {signal})
       .then((response) => response.json());
+    }
+
   };
