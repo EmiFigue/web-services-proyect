15a893642734dc731789c9a1b2a236041a1eb9af
Emifigueroa <emifigueroarojas@ciencias.unam.mx>
2024-10-04 12:33:48 -0600

    Funcional, manejo de errores y design

--- webservices/src/App.js
+++ webservices/src/App.js
@@ -4,16 +4,21 @@ import { searchTicket } from './components/interpretSearch';
 import Papa from 'papaparse';
 import csvStr from'./assets/tickets.csv';
 import { validarString } from './components/validarString';
+import iataStr from './assets/ciudades_iata.csv';
 
 function App() {
   const [search, setSearch] = useState("");
   const [weatherDeparture, setWeatherDeparture] = useState(null);
   const [weatherDestination, setWeatherDestination] = useState(null);
   const [isMetric, setIsMetric] = useState(true); // Unit toggle
-  const [isDayTime, setIsDayTime] = useState(true); // Day/night toggle
+  const [isDayTimeDeparture, setIsDayTimeDeparture] = useState(true);
+  const [isDayTimeDestination, setIsDayTimeDestination] = useState(true);
   const [isDarkMode, setIsDarkMode] = useState(false); // Dark mode toggle
   const [showPreferences, setShowPreferences] = useState(false); // Preferences modal
+  const [showDetailedInfo, setShowDetailedInfo] = useState(false);
+  const [isLargeFont, setIsLargeFont] = useState(false);
   const [tickets, setTickets] = useState({});
+  const [iata, setIata] = useState({});
   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
@@ -27,6 +32,17 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    Papa.parse(iataStr, {
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: (result) => {
+        setIata(result.data);
+      },
+    });
+  }, []);
+
 
   const handleInputChange = (e) => {
     setSearch(e.target.value.toUpperCase());
@@ -35,19 +51,21 @@ function App() {
   const searchPressed = () => {
     setErrorMessage(''); // Reset error message before new search
 
-    const result = searchTicket(search, tickets);
+    const result = searchTicket(search, tickets, iata);
     const valida = search
-    if(validarString(valida)==false) {
+    if(!validarString(valida)) {
       setErrorMessage('Input contiene caracteres inválidos, intente denuevo únicamente con letras y números');
       setWeatherDeparture(null);
       setWeatherDestination(null);
-    }
-    else if (result == undefined) {
+      return;
+    } else if (!result) {
       setErrorMessage('No existe el ticket buscado.');
       setWeatherDeparture(null);
       setWeatherDestination(null);
-    } else {
-      searchTicket(search, tickets)
+      return;
+    }
+    else {
+      searchTicket(search, tickets,iata)
         .then((res) => {
           if (Array.isArray(res)) {
             const [departureWeather, destinationWeather] = res;
@@ -56,14 +74,26 @@ function App() {
           } else {
             setWeatherDeparture(res);
             setWeatherDestination(null);
-          }
+          } 
         })
         .catch((error) => {
           console.error("Error fetching weather data: ", error);
-          setErrorMessage('Error fetching weather data.'); // Handle any other errors
+          setErrorMessage('La ciudad no existe o esta mal escrita. Intente de nuevo.'); 
         });
     }
   };
+
+  const calculateDayTime = (weather, setDayTime) => {
+    const currentTimeUTC = weather.dt;
+    const timezoneOffset = weather.timezone;
+    const localTime = currentTimeUTC + timezoneOffset;
+
+    const sunrise = weather.sys.sunrise + timezoneOffset;
+    const sunset = weather.sys.sunset + timezoneOffset;
+    const isDay = localTime >= sunrise && localTime < sunset;
+    setDayTime(isDay);
+  };
+  
   const toggleUnits = () => {
     setIsMetric(!isMetric);
   };
@@ -80,69 +110,65 @@ function App() {
     setShowPreferences(false);
   };
 
-  const calculateDayTime = (weather) => {
-    const currentTimeUTC = weather.dt;
-    const timezoneOffset = weather.timezone;
-    const localTime = currentTimeUTC + timezoneOffset;
-
-    const sunrise = weather.sys.sunrise + timezoneOffset;
-    const sunset = weather.sys.sunset + timezoneOffset;
-    setIsDayTime(localTime >= sunrise && localTime < sunset);
-  };
-
-  const renderWeather = (weather, locationName) => {
-    if (!weather || !weather.main || !weather.weather || !weather.weather[0]) return null;
-
-
-    calculateDayTime(weather);
-
-    return (
-      <div className="weather-info">
-        <h3>{locationName}: {weather.name}</h3>
-        <p>{weather.weather[0].description}</p>
-        <p>
-          Temperatura: {isMetric ? weather.main.temp : (weather.main.temp * 9 / 5 + 32).toFixed(2)}°{isMetric ? 'C' : 'F'}
-        </p>
-        <p>
-          Sensación térmica: {isMetric ? weather.main.feels_like : (weather.main.feels_like * 9 / 5 + 32).toFixed(2)}°{isMetric ? 'C' : 'F'}
-        </p>
-        <div className={`weather-icon-container ${isDayTime ? 'day' : 'night'}`}>
-          <img
-            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-            alt="Weather Icon"
-            className="weather-icon"
-          />
-        </div>
-      </div>
-    );
-  };
-
   return (
-    <div className={`App ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+    <div className={`App ${isDarkMode ? 'dark-mode' : 'light-mode'} ${isLargeFont ? 'large-font' : ''}`}>
       <div className="circle-bg-1"></div>
       <div className="circle-bg-2"></div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <header className="App-header">
-        <h2>AICM Clima</h2>
+        <h1>AICM Clima</h1>
       </header>
 
-      <button onClick={openPreferences} className="preferences-button">Preferencias</button>
+      <button onClick={openPreferences} 
+      className="preferences-button"
+      aria-label='Abrir el menú de preferencias'
+      >
+        Preferencias</button>
+      
 
       <input
+       
+        id='city-or-ticket'  
+        name='searchPlaceHolder'
         type='text'
         placeholder='Introduce ciudad o ticket'
-        onChange={handleInputChange} 
+        onChange={handleInputChange}
+        aria-label='Introduce el nombre de la ciudad, el código del aeropuerto (IATA), o el número de ticket para buscar el clima'
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            searchPressed();
+          }
+        }}
+        className={isLargeFont ? 'large-font-input' : ''}
       />
-      <button onClick={searchPressed}>Search</button>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
 
+      <button onClick={searchPressed}>
+        Search</button>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      {weatherDeparture && weatherDeparture.weather && weatherDeparture.weather[0] ? (weatherDeparture && <p>  </p> ) : (weatherDeparture && <p> La ciudad o IATA no existe o esta mal escrita. Intente de nuevo. </p>)}
+      <h4 className={isLargeFont ? 'large-font' : ''}>Busca el clima con el nombre de la ciudad, el código del aeropuerto (IATA) o por número de vuelo.</h4>
+
+      <div className="weather-container"></div>
       {/* Salida*/}
       {weatherDeparture && weatherDeparture.weather && weatherDeparture.weather[0] ? (
-        <div>
+        <div className="weather-info">
           <h3>Ciudad de salida: {weatherDeparture.name}</h3>
           <p>{weatherDeparture.weather[0].description}</p>
           <p>Temperatura: {weatherDeparture.main ? (isMetric ? weatherDeparture.main.temp : (weatherDeparture.main.temp * 9 / 5 + 32).toFixed(2)) : 'N/A'}°{isMetric ? 'C' : 'F'}</p>
           <p>Sensación térmica: {weatherDeparture.main ? (isMetric ? weatherDeparture.main.feels_like : (weatherDeparture.main.feels_like * 9 / 5 + 32).toFixed(2)) : 'N/A'}°{isMetric ? 'C' : 'F'}</p>
-          <div className={`weather-icon-container ${isDayTime ? 'day' : 'night'}`}>
+
+          {showDetailedInfo && (
+            <div className="detailed-weather-info">
+              <p>Humedad: {weatherDeparture.main.humidity}%</p>
+              <p>Presión atmosférica: {weatherDeparture.main.pressure ? (isMetric ? `${weatherDeparture.main.pressure} hPa` : `${(weatherDeparture.main.pressure * 0.0145038).toFixed(2)} PSI`) : 'N/A'}</p>
+              <p>Viento: {weatherDeparture.wind.speed ? (isMetric ? `${weatherDeparture.wind.speed} m/s` : `${(weatherDeparture.wind.speed * 2.23694).toFixed(2)} mph`) : 'N/A'}, Dirección: {weatherDeparture.wind.deg}°</p>
+              <p>Nubosidad: {weatherDeparture.clouds.all}%</p>
+              <p>Visibilidad: {weatherDeparture.visibility / 1000} km</p>
+            </div>
+          )}
+
+
+          <div className={`weather-icon-container ${isDayTimeDeparture ? 'day' : 'night'}`}>
             <img
               src={`https://openweathermap.org/img/wn/${weatherDeparture.weather[0].icon}@2x.png`}
               alt="Weather Icon"
@@ -151,17 +177,28 @@ function App() {
           </div>
         </div>
       ) : (
-        weatherDeparture && <p>No existe ciudad buscada.</p>
+        weatherDeparture && <p>  </p> 
       )}
 
       {/* Llegada */}
       {weatherDestination && weatherDestination.weather && weatherDestination.weather[0] ? (
-        <div>
+        <div className={`weather-info ${isLargeFont ? 'large-font' : ''}`}>
           <h3>Ciudad de destino: {weatherDestination.name}</h3>
           <p>{weatherDestination.weather[0].description}</p>
           <p>Temperatura: {weatherDestination.main ? (isMetric ? weatherDestination.main.temp : (weatherDestination.main.temp * 9 / 5 + 32).toFixed(2)) : 'N/A'}°{isMetric ? 'C' : 'F'}</p>
           <p>Sensación térmica: {weatherDestination.main ? (isMetric ? weatherDestination.main.feels_like : (weatherDestination.main.feels_like * 9 / 5 + 32).toFixed(2)) : 'N/A'}°{isMetric ? 'C' : 'F'}</p>
-          <div className={`weather-icon-container ${isDayTime ? 'day' : 'night'}`}>
+
+          {showDetailedInfo && (
+            <div className="detailed-weather-info">
+              <p>Humedad: {weatherDestination.main.humidity}%</p>
+              <p>Presión atmosférica: {weatherDestination.main.pressure ? (isMetric ? `${weatherDestination.main.pressure} hPa` : `${(weatherDestination.main.pressure * 0.0145038).toFixed(2)} PSI`) : 'N/A'}</p>
+              <p>Viento: {weatherDestination.wind.speed ? (isMetric ? `${weatherDestination.wind.speed} m/s` : `${(weatherDestination.wind.speed * 2.23694).toFixed(2)} mph`) : 'N/A'}, Dirección: {weatherDestination.wind.deg}°</p>
+              <p>Nubosidad: {weatherDestination.clouds.all}%</p>
+              <p>Visibilidad: {weatherDestination.visibility / 1000} km</p>
+            </div>
+          )}
+
+          <div className={`weather-icon-container ${isDayTimeDestination ? 'day' : 'night'}`}>
             <img
               src={`https://openweathermap.org/img/wn/${weatherDestination.weather[0].icon}@2x.png`}
               alt="Weather Icon"
@@ -170,8 +207,10 @@ function App() {
           </div>
         </div>
       ) : (
-        weatherDestination && <p>No se pudo obtener el clima de destino.</p>
+        weatherDestination && <p>  </p>
+
       )}
+
 
       {showPreferences && (
         <div className="modal-overlay">
@@ -196,7 +235,28 @@ function App() {
               <span className="preference-label">Imperial</span>
             </div>
 
-            <button onClick={closePreferences} className="close-button">Cerrar</button>
+            <div className="preference-item">
+              <span className="preference-label">Información detallada</span>
+              <label className="switch">
+                <input type="checkbox" checked={showDetailedInfo} onChange={() => setShowDetailedInfo(prev => !prev)} />
+                <span className="slider round"></span>
+              </label>
+            </div>
+
+            <div className="preference-item">
+              <span className="preference-label">Letra grande</span>
+              <label className="switch">
+                <input type="checkbox" checked={isLargeFont} onChange={() => setIsLargeFont(prev => !prev)} />
+                <span className="slider round"></span>
+              </label>
+            </div>
+
+            <button onClick={closePreferences}
+            className="close-button"
+            aria-label='Cerrar el menú de preferencias'
+            >
+              Cerrar</button>
+
           </div>
         </div>
       )}
--- webservices/src/components/interpretSearch.js
+++ webservices/src/components/interpretSearch.js
@@ -21,7 +21,20 @@ function searchInArray (search, tickets){
   return { latDep: null, lonDep: null, latDest: null, lonDest: null };
 }
 
-export function searchTicket(search, tickets) {
+
+function searchIata (search,iata){
+  let ciudad = "";
+
+  for (let i = 0; i<iata.length; i++){
+    if (search === iata[i].iata){
+      ciudad = iata[i].ciudad
+      return ciudad;
+    }
+  }
+  return {ciudad: null}
+}
+
+export function searchTicket(search, tickets, iata) {
   if (isTicket(search)) {
     const { latDep, lonDep, latDest, lonDest } = searchInArray(search, tickets);
 
@@ -33,9 +46,15 @@ export function searchTicket(search, tickets) {
     } else {
       return null;
     }
-  } else {
+  } else if (isIata(search)){
+    const ciudad = searchIata(search, iata);
+    if (ciudad){
+      return fetchWeather(ciudad);
+    }else{
+      return false
+    }
+  }else {
     const { latDep, lonDep, latDest, lonDest } = searchInArray(search, tickets);
-    
     if (latDep && lonDep) {
       return fetchWeatherLat(latDep, lonDep);
     } else {
@@ -50,6 +69,11 @@ function isTicket(search) {
   const hasNumber = /[0-9]/.test(search);
   
   return isAlphaNumeric && hasLetter && hasNumber && search.length === 6;
+}
+
+function isIata(search) {
+  const hasLetter = /[A-Za-z]/.test(search);
+  return hasLetter && search.length === 3;
 }
 
 export default isTicket;
